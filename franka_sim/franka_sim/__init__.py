fa9c807f70e5d86e91cfc1160c4204ad05c74c7d
Zheyuan Hu <huzheyuan428@gmail.com>
2023-12-08 15:30:33 -0800

    add mujoco base env and franka lift cube gym env.

---
+++ franka_sim/franka_sim/__init__.py
@@ -0,0 +1,18 @@
+from franka_sim.mujoco_gym_env import MujocoGymEnv, GymRenderingSpec
+__all__ = [
+    "MujocoGymEnv",
+    "GymRenderingSpec",
+]
+
+from gymnasium.envs.registration import register
+register(
+    id='PandaPickCube-v0',
+    entry_point='franka_sim.envs:PandaPickCubeGymEnv',
+    max_episode_steps=100,
+)
+register(
+    id='PandaPickCubeVision-v0',
+    entry_point='franka_sim.envs:PandaPickCubeGymEnv',
+    max_episode_steps=100,
+    kwargs={'image_obs': True},
+)
---
+++ franka_sim/franka_sim/envs/__init__.py
@@ -0,0 +1,5 @@
+from franka_sim.envs.panda_pick_gym_env import PandaPickCubeGymEnv
+
+__all__ = [
+    "PandaPickCubeGymEnv",
+]
