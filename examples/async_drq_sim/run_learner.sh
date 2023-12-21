9169090e9045ad5c6e5e2fbf22da47da14fa2135
Zheyuan Hu <huzheyuan428@gmail.com>
2023-12-21 15:04:09 -0800

    add drq example in sim with lift cube env

---
+++ examples/async_drq_sim/run_learner.sh
@@ -0,0 +1,13 @@
+export XLA_PYTHON_CLIENT_PREALLOCATE=false && \
+export XLA_PYTHON_CLIENT_MEM_FRACTION=.1 && \
+python async_drq_sim.py \
+    --learner \
+    --env PandaPickCubeVision-v0 \
+    --exp_name=serl_dev_drq_sim_test_mobilenet_s_stopgrad \
+    --seed 0 \
+    --random_steps 1000 \
+    --training_starts 1000 \
+    --utd_ratio 4 \
+    --batch_size 256 \
+    --eval_period 2000 \
+    --debug
