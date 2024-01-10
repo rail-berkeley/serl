db1ee6ca7067239d043346bcd70663db63f8335c
Zheyuan Hu <huzheyuan428@gmail.com>
2024-01-09 16:26:58 -0800

    add __main__ to record demo example script

--- examples/async_peg_insert_drq/record_demo.py
+++ examples/async_peg_insert_drq/record_demo.py
@@ -2,6 +2,8 @@ import gymnasium as gym
 from tqdm import tqdm
 import numpy as np
 import copy
+import pickle as pkl
+import datetime
 
 import franka_env
 
@@ -16,50 +18,47 @@ from serl_launcher.wrappers.serl_obs_wrappers import SERLObsWrapper
 
 from jaxrl_m.envs.wrappers.chunking import ChunkingWrapper
 
-env = gym.make("FrankaRobotiqPegInsert-Vision-v0")
-env = GripperCloseEnv(env)
-env = SpacemouseIntervention(env)
-env = RelativeFrame(env)
-env = Quat2EulerWrapper(env)
-env = SERLObsWrapper(env)
-env = ChunkingWrapper(env, obs_horizon=1, act_exec_horizon=None)
+if __name__ == "__main__":
+    env = gym.make("FrankaRobotiqPegInsert-Vision-v0")
+    env = GripperCloseEnv(env)
+    env = SpacemouseIntervention(env)
+    env = RelativeFrame(env)
+    env = Quat2EulerWrapper(env)
+    env = SERLObsWrapper(env)
+    env = ChunkingWrapper(env, obs_horizon=1, act_exec_horizon=None)
 
-obs, _ = env.reset()
+    obs, _ = env.reset()
 
-transitions = []
-success_count = 0
-success_needed = 20
-pbar = tqdm(total=success_needed)
+    transitions = []
+    success_count = 0
+    success_needed = 20
+    pbar = tqdm(total=success_needed)
 
-while success_count < success_needed:
-    next_obs, rew, done, truncated, info = env.step(action=np.zeros((6,)))
-    actions = info["intervene_action"]
+    while success_count < success_needed:
+        next_obs, rew, done, truncated, info = env.step(action=np.zeros((6,)))
+        actions = info["intervene_action"]
 
-    transition = copy.deepcopy(
-        dict(
-            observations=obs,
-            actions=actions,
-            next_observations=next_obs,
-            rewards=rew,
-            masks=1.0 - done,
-            dones=done,
+        transition = copy.deepcopy(
+            dict(
+                observations=obs,
+                actions=actions,
+                next_observations=next_obs,
+                rewards=rew,
+                masks=1.0 - done,
+                dones=done,
+            )
         )
-    )
-    transitions.append(transition)
+        transitions.append(transition)
 
-    obs = next_obs
+        obs = next_obs
 
-    if done:
-        obs, _ = env.reset()
-        success_count += 1
-        pbar.update(1)
+        if done:
+            obs, _ = env.reset()
+            success_count += 1
+            pbar.update(1)
 
-
-import pickle as pkl
-import datetime
-
-uuid = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-file_name = f"peg_insert_{success_needed}_demos_{uuid}.pkl"
-with open(file_name, "wb") as f:
-    pkl.dump(transitions, f)
-    print(f"saved {success_needed} demos to {file_name}")
+    uuid = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    file_name = f"peg_insert_{success_needed}_demos_{uuid}.pkl"
+    with open(file_name, "wb") as f:
+        pkl.dump(transitions, f)
+        print(f"saved {success_needed} demos to {file_name}")
