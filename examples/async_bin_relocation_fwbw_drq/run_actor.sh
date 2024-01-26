808d60f72a051f241b844574916595766bfd05ff
Zheyuan Hu <huzheyuan428@gmail.com>
2024-01-26 14:54:44 -0800

    add fwbw training with binary reward classifier

---
+++ examples/async_bin_relocation_fwbw_drq/run_actor.sh
@@ -0,0 +1,19 @@
+export XLA_PYTHON_CLIENT_PREALLOCATE=false && \
+export XLA_PYTHON_CLIENT_MEM_FRACTION=.3 && \
+python async_drq_randomized.py "$@" \
+    --actor \
+    --render \
+    --env FrankaBinRelocation-Vision-v0 \
+    --exp_name=serl_dev_drq_rlpd20demos_bin_fwbw_resnet_096 \
+    --seed 0 \
+    --random_steps 200 \
+    --training_starts 200 \
+    --utd_ratio 4 \
+    --batch_size 256 \
+    --eval_period 2000 \
+    --encoder_type resnet-pretrained \
+    --demo_path fw_bin_2000_demo_2024-01-23_18-49-56.pkl \
+    --fw_ckpt_path /home/undergrad/code/serl_dev/examples/async_bin_relocation_fwbw_drq/bin_fw_096 \
+    --bw_ckpt_path /home/undergrad/code/serl_dev/examples/async_bin_relocation_fwbw_drq/bin_bw_096 \
+    --eval_checkpoint_step 31000 \
+    --eval_n_trajs 100
