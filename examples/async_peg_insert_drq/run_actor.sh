82f1924217d5665ca3c92cb05370d6db56f1b631
Zheyuan Hu <huzheyuan428@gmail.com>
2024-01-28 20:58:18 -0800

    update eval code and script for peg insert

--- examples/async_peg_insert_drq/run_actor.sh
+++ examples/async_peg_insert_drq/run_actor.sh
@@ -12,4 +12,7 @@ python async_drq_randomized.py "$@" \
     --batch_size 256 \
     --eval_period 2000 \
     --encoder_type resnet-pretrained \
-    --demo_path peg_insert_20_demos_2023-12-25_16-13-25.pkl
+    --demo_path peg_insert_20_demos_2023-12-25_16-13-25.pkl \
+    # --checkpoint_path /home/undergrad/code/serl_dev/examples/async_pcb_insert_drq/5x5_20degs_100demos_rand_pcb_insert_bc \
+    # --eval_checkpoint_step 20000 \
+    # --eval_n_trajs 100 \
