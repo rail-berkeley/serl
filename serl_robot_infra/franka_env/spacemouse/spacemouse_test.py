e7b6d769f99f1d82e35489890e29d35f7536b64b
Gautam Salhotra <gautams3@users.noreply.github.com>
2024-02-06 15:59:00 -0800

    update to match request formatting and coding style

--- serl_robot_infra/franka_env/spacemouse/spacemouse_test.py
+++ serl_robot_infra/franka_env/spacemouse/spacemouse_test.py
@@ -1,12 +1,21 @@
-# Test the SpaceMouseExpert class
+""" Test the spacemouse output. """
 
 import time
-from serl_robot_infra.franka_env.spacemouse.spacemouse_expert import SpaceMouseExpert
 import numpy as np
+from serl_robot_infra.franka_env.spacemouse.spacemouse_expert import SpaceMouseExpert
 np.set_printoptions(precision=3, suppress=True)
 
-spacemouse = SpaceMouseExpert()
-while True:
-    action, buttons = spacemouse.get_action()
-    print(f'Spacemouse action: {action}, buttons: {buttons}')
-    time.sleep(0.1)
+def test_spacemouse():
+    """ Test the SpaceMouseExpert class. """
+    spacemouse = SpaceMouseExpert()
+    while True:
+        action, buttons = spacemouse.get_action()
+        print(f'Spacemouse action: {action}, buttons: {buttons}')
+        time.sleep(0.1)
+
+def main():
+    """ Call spacemouse test."""
+    test_spacemouse()
+
+if __name__ == '__main__':
+    main()
