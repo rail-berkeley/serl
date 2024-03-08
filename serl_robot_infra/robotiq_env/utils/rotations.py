1156d6555899584e9b818fbbdc3c47575302b9f6
nisutte <nisutte@student.ethz.ch>
2024-03-08 17:10:25 +0100

    typo

--- serl_robot_infra/robotiq_env/utils/rotations.py
+++ serl_robot_infra/robotiq_env/utils/rotations.py
@@ -1,11 +1,14 @@
+import numpy as np
 from scipy.spatial.transform import Rotation as R
 
 """
 Robotiq UR5 represents the orientation in axis angle representation
 """
 
-def rotvev_2_quat(rotvec):
+
+def rotvec_2_quat(rotvec):
     return R.from_rotvec(rotvec).as_quat()
+
 
 def quat_2_rotvec(quat):
     return R.from_quat(quat).as_rotvec()
