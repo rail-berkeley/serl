8d63a85f0351b7117c3ae1d94a850107531e956e
nisutte <nisutte@student.ethz.ch>
2024-03-15 15:58:49 +0100

    deadspace and z-rot-invariant implementation

--- serl_robot_infra/robotiq_env/envs/wrappers.py
+++ serl_robot_infra/robotiq_env/envs/wrappers.py
@@ -9,13 +9,14 @@ class SpacemouseIntervention(gym.ActionWrapper):
     def __init__(self, env):
         super().__init__(env)
 
-        self.gripper_enabled = False
+        self.gripper_enabled = True
 
         self.expert = SpaceMouseExpert()
         self.last_intervene = 0
         self.left, self.right = False, False
 
-        self.invert_axes = [1, 1, 1, 1, 1, 1]
+        self.invert_axes = [-1, -1, 1, 1, -1, 1]
+        self.deadspace = 0.15
 
     def action(self, action: np.ndarray) -> np.ndarray:
         """
@@ -24,30 +25,31 @@ class SpacemouseIntervention(gym.ActionWrapper):
         Output:
         - action: spacemouse action if nonezero; else, policy action
         """
-        expert_a, buttons = self.expert.get_action()
-        self.left, self.right = tuple(buttons)
+        expert_a = self.get_deadspace_action()
 
         if np.linalg.norm(expert_a) > 0.001:
             self.last_intervene = time.time()
 
         if self.gripper_enabled:
-            if self.left:  # close gripper
-                gripper_action = np.random.uniform(-1, -0.9, size=(1,))
-                self.last_intervene = time.time()
-            elif self.right:  # open gripper
-                gripper_action = np.random.uniform(0.9, 1, size=(1,))
-                self.last_intervene = time.time()
-            else:
-                gripper_action = np.zeros((1,))
+            gripper_action = np.zeros((1,)) - int(self.left) + int(self.right)
             expert_a = np.concatenate((expert_a, gripper_action), axis=0)
 
         if time.time() - self.last_intervene < 0.5:
             expert_a = self.adapt_spacemouse_output(expert_a)
-            return np.concatenate((expert_a, np.array([0])))    # TODO add gripper
+            return expert_a
 
         return action
 
-    def adapt_spacemouse_output(self, expert_a: np.ndarray) -> np.ndarray:
+    def get_deadspace_action(self) -> np.ndarray:
+        expert_a, buttons = self.expert.get_action()
+
+        expert_a = np.clip((expert_a - np.sign(expert_a) * self.deadspace) / (1.-self.deadspace), a_min=-1.0, a_max=1.)
+
+        self.left, self.right = tuple(buttons)
+
+        return np.array(expert_a, dtype=np.float32)
+
+    def adapt_spacemouse_output(self, action: np.ndarray) -> np.ndarray:
         """
         Input:
         - expert_a: spacemouse raw output
@@ -55,18 +57,19 @@ class SpacemouseIntervention(gym.ActionWrapper):
         - expert_a: spacemouse output adapted to force space (action)
         """
 
-        position = [1.]*6       # TODO get position
+        position = super().get_wrapper_attr("currpos")  # get position from robotiq_env
         z_angle = np.arctan2(position[1], position[0])  # get first joint angle
 
         z_rot = R.from_rotvec(np.array([0, 0, z_angle]))
-        expert_a *= self.invert_axes  # if some want to be inverted
-        expert_a[:3] = z_rot.apply(expert_a[:3])  # z rotation invariant translation
-        expert_a[3:] = z_rot.apply(expert_a[3:])  # z rotation invariant rotation
+        action[:6] *= self.invert_axes  # if some want to be inverted
+        action[:3] = z_rot.apply(action[:3])  # z rotation invariant translation
+        action[3:6] = z_rot.inv().apply(action[3:6])  # z rotation invariant rotation
 
-        return expert_a
+        return action
 
-    def step(self, action):
+    def step(self, action):  # TODO change here!
         new_action = self.action(action)
+        # print(f"new action: {new_action}")
         obs, rew, done, truncated, info = self.env.step(new_action)
         info["intervene_action"] = new_action
         info["left"] = self.left
