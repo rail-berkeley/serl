0f48066263966d3b43bf2eed7a97ae63ac8ce016
nisutte <nisutte@student.ethz.ch>
2024-03-15 15:58:28 +0100

    remove overwrite example

--- serl_robot_infra/robotiq_env/envs/test_env/robotiq_test.py
+++ serl_robot_infra/robotiq_env/envs/test_env/robotiq_test.py
@@ -5,7 +5,3 @@ from robotiq_env.envs.test_env.config import TestConfig
 class RobotiqTest(RobotiqEnv):
     def __init__(self, **kwargs):
         super().__init__(**kwargs, config=TestConfig)
-
-    def go_to_rest(self, joint_reset=False):
-        # how to overwrite without blocking the old function
-        super().go_to_rest(joint_reset)
