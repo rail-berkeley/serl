92d2c729cdb2627111102c5da8b105f8ccdbe36d
nisutte <nisutte@student.ethz.ch>
2024-03-28 17:57:05 +0100

    new actor and learner parameters for now

--- examples/robotiq_sac/run_actor.sh
+++ examples/robotiq_sac/run_actor.sh
@@ -1,13 +1,15 @@
 export XLA_PYTHON_CLIENT_PREALLOCATE=false && \
-export XLA_PYTHON_CLIENT_MEM_FRACTION=.05 && \
+export XLA_PYTHON_CLIENT_MEM_FRACTION=.3 && \
 python sac_policy_robotiq.py "$@" \
     --actor \
     --env robotiq-grip-v1 \
     --exp_name=sac_robotiq_policy \
     --seed 0 \
-    --random_steps 1000 \
-    --training_starts 1000 \
+    --max_steps 10000 \
+    --random_steps 600 \
+    --training_starts 600 \
     --utd_ratio 8 \
     --batch_size 256 \
-    --eval_period 2000
+    --eval_period 2000 \
+    --demo_paths "/home/nico/real-world-rl/serl/examples/robotiq_bc/robotiq_test_20_demos_2024-03-26_12-23-50.pkl" \
 #    --debug
--- examples/robotiq_sac/run_learner.sh
+++ examples/robotiq_sac/run_learner.sh
@@ -1,13 +1,16 @@
 export XLA_PYTHON_CLIENT_PREALLOCATE=false && \
-export XLA_PYTHON_CLIENT_MEM_FRACTION=.05 && \
+export XLA_PYTHON_CLIENT_MEM_FRACTION=.3 && \
 python sac_policy_robotiq.py "$@" \
     --learner \
     --env robotiq-grip-v1 \
     --exp_name=sac_robotiq_policy \
     --seed 0 \
-    --random_steps 1000 \
-    --training_starts 1000 \
+    --random_steps 600 \
+    --training_starts 600 \
     --utd_ratio 8 \
-    --batch_size 256 \
-    --eval_period 2000
+    --batch_size 1024 \
+    --eval_period 2000 \
+    --max_steps 10000 \
+    --preload_rlds_path "/home/nico/real-world-rl/serl/examples/robotiq_sac/rlds" \
+#    --demo_paths "/home/nico/real-world-rl/serl/examples/robotiq_bc/robotiq_test_20_demos_2024-03-26_12-23-50.pkl" \
 #    --debug
