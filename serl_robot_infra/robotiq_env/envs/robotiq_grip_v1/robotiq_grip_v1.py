d85b9610c7a5d340e0444992d770483f16519542
nisutte <nisutte@student.ethz.ch>
2024-04-03 16:12:36 +0200

    new reward function

--- serl_robot_infra/robotiq_env/envs/robotiq_grip_v1/robotiq_grip_v1.py
+++ serl_robot_infra/robotiq_env/envs/robotiq_grip_v1/robotiq_grip_v1.py
@@ -7,7 +7,13 @@ class RobotiqGripV1(RobotiqEnv):
         super().__init__(**kwargs, config=RobotiqCornerConfig)
 
     def compute_reward(self, obs) -> float:
-        if int(self.gripper_state[1]) == 1 and 10 < self.gripper_state[0] < 30 and self.curr_force[2] < -1.:
-            return 1.
+        if int(self.gripper_state[1]) == 1:
+            if 10 < self.gripper_state[0] < 30:
+                if self.curr_force[2] < -1.:
+                    return 10.
+                return 5.
+            return 2.5
+        if self.curr_force[2] > 5.:
+            return (5. - self.curr_force[2]) * 0.2          # return 0 if force=5, -1 if force=10 (max))
         else:
             return 0.0
