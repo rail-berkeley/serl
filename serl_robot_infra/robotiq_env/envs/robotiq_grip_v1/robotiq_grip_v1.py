8a4fe9b7373e23e8bc436cdd7d416a4959a4c9b3
nisutte <nisutte@student.ethz.ch>
2024-04-04 14:37:27 +0200

    streamlined reward function

--- serl_robot_infra/robotiq_env/envs/robotiq_grip_v1/robotiq_grip_v1.py
+++ serl_robot_infra/robotiq_env/envs/robotiq_grip_v1/robotiq_grip_v1.py
@@ -7,13 +7,9 @@ class RobotiqGripV1(RobotiqEnv):
         super().__init__(**kwargs, config=RobotiqCornerConfig)
 
     def compute_reward(self, obs) -> float:
-        if int(self.gripper_state[1]) == 1:
-            if 10 < self.gripper_state[0] < 30:
-                if self.curr_force[2] < -1.:
-                    return 10.
-                return 5.
-            return 2.5
+        if 10. < self.gripper_state[0] < 30. and self.curr_force[2] < -3.:
+            return 1.
         if self.curr_force[2] > 5.:
-            return (5. - self.curr_force[2]) * 0.2          # return 0 if force=5, -1 if force=10 (max))
+            return (5. - self.curr_force[2]) * 0.1          # return 0 if force=5, -0.5 if force=10 (max))
         else:
             return 0.0
