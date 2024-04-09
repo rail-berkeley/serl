17d1fabc042b53327549624ea9e9529f93f182ec
nisutte <nisutte@student.ethz.ch>
2024-04-09 16:40:32 +0200

    new actor and learner config

--- examples/robotiq_sac/run_actor.sh
+++ examples/robotiq_sac/run_actor.sh
@@ -4,12 +4,12 @@ python sac_policy_robotiq.py "$@" \
     --actor \
     --env robotiq-grip-v1 \
     --exp_name=sac_robotiq_policy \
-    --seed 0 \
+    --seed 42 \
     --max_steps 10000 \
     --random_steps 0 \
     --training_starts 0 \
     --utd_ratio 8 \
     --batch_size 1024 \
-    --eval_period 1000 \
+    --eval_period 500 \
     --reward_scale 1 \
 #    --debug
--- examples/robotiq_sac/run_learner.sh
+++ examples/robotiq_sac/run_learner.sh
@@ -4,12 +4,12 @@ python sac_policy_robotiq.py "$@" \
     --learner \
     --env robotiq-grip-v1 \
     --exp_name=sac_robotiq_policy \
-    --seed 0 \
+    --seed 42 \
     --random_steps 400 \
     --training_starts 400 \
     --utd_ratio 8 \
     --batch_size 1024 \
-    --eval_period 1000 \
+    --eval_period 500 \
     --max_steps 100000 \
     --reward_scale 1 \
     --demo_paths "/home/nico/real-world-rl/serl/examples/robotiq_bc/robotiq_test_20_demos_apr9_action_cost.pkl" \
