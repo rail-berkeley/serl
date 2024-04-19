30e3a6ab286a23037635598867b706d25d510801
nisutte <nisutte@student.ethz.ch>
2024-04-19 16:30:12 +0200

    added suction cost

--- serl_robot_infra/robotiq_env/envs/robotiq_grip_v1/robotiq_grip_v1.py
+++ serl_robot_infra/robotiq_env/envs/robotiq_grip_v1/robotiq_grip_v1.py
@@ -4,6 +4,11 @@ from robotiq_env.envs.robotiq_env import RobotiqEnv
 from robotiq_env.envs.robotiq_grip_v1.config import RobotiqCornerConfig
 
 
+# used for float value comparisons (pressure of vacuum-gripper)
+def is_close(value, target):
+    return abs(value - target) < 1e-4
+
+
 class RobotiqGripV1(RobotiqEnv):
     def __init__(self, **kwargs):
         super().__init__(**kwargs, config=RobotiqCornerConfig)
@@ -11,8 +16,10 @@ class RobotiqGripV1(RobotiqEnv):
     def compute_reward(self, obs, action) -> float:
         # huge action gives negative reward (like in mountain car)
         action_cost = 0.1 * np.sum(np.power(action, 2))
-        # TODO maybe add neg reward for sucking too early
         step_cost = 0.01
+
+        gripper_state = obs["state"]['gripper_state']
+        suck_cost = 0.1 * float(is_close(gripper_state[0], 0.99))
 
         pose = obs["state"]["tcp_pose"]
         # box_xy = np.array([0.009, -0.5437])     # TODO replace with camera / pointcloud info of box
@@ -20,9 +27,9 @@ class RobotiqGripV1(RobotiqEnv):
 
         # print(f"action_cost: {action_cost}, xy_cost: {xy_cost}")
         if self.reached_goal_state(obs):
-            return 10. - action_cost - step_cost
+            return 10. - action_cost - step_cost - suck_cost
         else:
-            return 0.0 - action_cost - step_cost
+            return 0.0 - action_cost - step_cost - suck_cost
 
     def reached_goal_state(self, obs) -> bool:
         # obs[0] == gripper pressure, obs[4] == force in Z-axis
