7e7f984de240bd5063730362007d27283943e815
nisutte <nisutte@student.ethz.ch>
2024-04-19 16:30:36 +0200

    new actor and learner params

--- examples/robotiq_sac/run_learner.sh
+++ examples/robotiq_sac/run_learner.sh
@@ -4,14 +4,13 @@ python sac_policy_robotiq.py "$@" \
     --learner \
     --env robotiq-grip-v1 \
     --exp_name=sac_robotiq_policy \
+    --max_traj_length 300 \
     --seed 42 \
-    --random_steps 400 \
-    --training_starts 400 \
+    --training_starts 900 \
     --utd_ratio 8 \
-    --batch_size 1024 \
-    --eval_period 500 \
-    --max_steps 100000 \
+    --batch_size 2048 \
+    --max_steps 50000 \
     --reward_scale 1 \
-    --demo_paths "/home/nico/real-world-rl/serl/examples/robotiq_bc/robotiq_test_20_demos_apr9_action_cost.pkl" \
+    --demo_paths "/home/nico/real-world-rl/serl/examples/robotiq_sac/robotiq_test_20_demos_apr11_random_boxes.pkl" \
 #    --preload_rlds_path "/home/nico/real-world-rl/serl/examples/robotiq_sac/rlds" \
 #    --debug
