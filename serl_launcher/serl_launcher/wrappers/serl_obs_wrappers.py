089199bfd4a66ec00501e85aa7452908577556fd
nisutte <nisutte@student.ethz.ch>
2024-05-03 11:57:41 +0200

    added wrapper to scale the obs space

--- serl_launcher/serl_launcher/wrappers/serl_obs_wrappers.py
+++ serl_launcher/serl_launcher/wrappers/serl_obs_wrappers.py
@@ -1,10 +1,12 @@
 import gymnasium as gym
+from gymnasium import spaces
+from gymnasium.core import ObsType, WrapperObsType
 from gymnasium.spaces import flatten_space, flatten
 
 
 class SERLObsWrapper(gym.ObservationWrapper):
     """
-    This observation wrapper treat the observation space as a dictionary
+    This observation wrapper treats the observation space as a dictionary
     of a flattened state space and the images.
     """
 
@@ -27,8 +29,8 @@ class SERLObsWrapper(gym.ObservationWrapper):
 
 class SerlObsWrapperNoImages(gym.ObservationWrapper):
     """
-    This observation wrapper treat the observation space as a flattened state
-     space, if no images are present.
+    This observation wrapper treats the observation space as a flattened state
+    space, if no images are present.
     """
 
     def __init__(self, env):
@@ -37,4 +39,37 @@ class SerlObsWrapperNoImages(gym.ObservationWrapper):
 
     def observation(self, obs):
         obs = flatten(self.env.observation_space["state"], obs["state"])
+        return obs
+
+
+class ScaleObservationWrapper(gym.ObservationWrapper):
+
+    """
+    This observation wrapper scales the observations with the provided hyperparams
+    (to somewhat normalize the observations space)
+    """
+    def __init__(self,
+                 env,
+                 translation_scale=10.,
+                 rotation_scale=10.,
+                 velocity_scale=10.,
+                 rotational_velocity_scale=10.,
+                 force_scale=1.,
+                 torque_scale=10.
+                 ):
+        super().__init__(env)
+        self.translation_scale = translation_scale
+        self.rotation_scale = rotation_scale
+        self.velocity_scale = velocity_scale
+        self.rotational_velocity_scale = rotational_velocity_scale
+        self.force_scale = force_scale
+        self.torque_scale = torque_scale
+
+    def observation(self, obs) -> WrapperObsType:
+        obs["state"]["tcp_pose"][:3] *= self.translation_scale
+        obs["state"]["tcp_pose"][3:] *= self.rotation_scale
+        obs["state"]["tcp_vel"][:3] *= self.velocity_scale
+        obs["state"]["tcp_vel"][3:] *= self.rotational_velocity_scale
+        obs["state"]["tcp_force"] *= self.force_scale
+        obs["state"]["tcp_torque"] *= self.torque_scale
         return obs
