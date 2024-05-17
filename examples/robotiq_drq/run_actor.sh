78feccad9c6b7bcb9f0f4e9d947b0b9b926b280b
nisutte <nisutte@student.ethz.ch>
2024-05-17 14:03:08 +0200

    run that make drq work

--- examples/robotiq_drq/run_actor.sh
+++ examples/robotiq_drq/run_actor.sh
@@ -1,16 +1,16 @@
 export XLA_PYTHON_CLIENT_PREALLOCATE=false && \
-export XLA_PYTHON_CLIENT_MEM_FRACTION=.1 && \
+export XLA_PYTHON_CLIENT_MEM_FRACTION=.2 && \
 python drq_policy_robotiq.py "$@" \
     --actor \
     --env robotiq_camera_env \
     --exp_name=drq_robotiq_policy \
     --max_traj_length 100 \
     --camera_mode rgb \
-    --seed 0 \
-    --max_steps 10000 \
-    --random_steps 0 \
-    --training_starts 0 \
-    --utd_ratio 4 \
+    --seed 2 \
+    --max_steps 20000 \
+    --random_steps 100 \
+    --training_starts 100 \
+    --utd_ratio 8 \
     --batch_size 128 \
     --eval_period 1000 \
     --encoder_type resnet-pretrained \
--- examples/robotiq_drq/run_learner.sh
+++ examples/robotiq_drq/run_learner.sh
@@ -6,14 +6,14 @@ python drq_policy_robotiq.py "$@" \
     --exp_name=drq_robotiq_policy \
     --camera_mode rgb \
     --max_traj_length 100 \
-    --seed 0 \
+    --seed 2 \
     --max_steps 20000 \
-    --random_steps 0 \
-    --training_starts 0 \
-    --utd_ratio 4 \
+    --random_steps 100 \
+    --training_starts 100 \
+    --utd_ratio 8 \
     --batch_size 128 \
     --eval_period 1000 \
     --encoder_type resnet-pretrained \
-    --checkpoint_period 10000 \
-    --demo_path /home/nico/real-world-rl/serl/examples/robotiq_drq/pcb_insert_20_demos_apr30_newrew.pkl \
-    --debug
+    --checkpoint_period 5000 \
+    --demo_path /home/nico/real-world-rl/serl/examples/robotiq_drq/pcb_insert_20_demos_mai16_100transition.pkl \
+#    --debug
