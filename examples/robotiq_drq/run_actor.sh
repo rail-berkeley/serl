4b5209c402e522da04babf61cd4fde9a896bb349
nisutte <nisutte@student.ethz.ch>
2024-06-06 18:36:20 +0200

    new training and actor params for ResNet18

--- examples/robotiq_drq/run_actor.sh
+++ examples/robotiq_drq/run_actor.sh
@@ -1,17 +1,18 @@
+sh ~/stop.sh && \
 export XLA_PYTHON_CLIENT_PREALLOCATE=false && \
-export XLA_PYTHON_CLIENT_MEM_FRACTION=.2 && \
+export XLA_PYTHON_CLIENT_MEM_FRACTION=.1 && \
 python drq_policy_robotiq.py "$@" \
     --actor \
     --env robotiq_camera_env \
-    --exp_name=drq_robotiq_policy \
     --max_traj_length 100 \
+    --exp_name=drq_rgb_5box \
     --camera_mode rgb \
-    --seed 2 \
-    --max_steps 20000 \
-    --random_steps 100 \
-    --training_starts 100 \
+    --seed 1 \
+    --max_steps 30000 \
+    --random_steps 500 \
+    --training_starts 500 \
     --utd_ratio 8 \
     --batch_size 128 \
-    --eval_period 1000 \
-    --encoder_type resnet-pretrained \
+    --eval_period 2500 \
+    --encoder_type resnet-pretrained-18 \
 #    --debug
--- examples/robotiq_drq/run_evaluation.sh
+++ examples/robotiq_drq/run_evaluation.sh
@@ -1,13 +1,13 @@
 export XLA_PYTHON_CLIENT_PREALLOCATE=false && \
-export XLA_PYTHON_CLIENT_MEM_FRACTION=.3 && \
+export XLA_PYTHON_CLIENT_MEM_FRACTION=.2 && \
 python drq_policy_robotiq.py "$@" \
     --actor \
     --env robotiq_camera_env \
-    --exp_name=drq_robotiq_policy_evaluation \
+    --exp_name=drq_evaluation \
     --max_traj_length 100 \
-    --camera_mode depth \
-    --checkpoint_path "/home/nico/real-world-rl/serl/examples/robotiq_drq/checkpoints 0523 rgb stable pooling=avg"\
+    --camera_mode rgb \
+    --checkpoint_path "/home/nico/real-world-rl/serl/examples/robotiq_drq/checkpoints 0606-16:05 rgb Res18 avg"\
     --eval_checkpoint_step 20000 \
-    --encoder_type resnet-pretrained \
-    --eval_n_trajs 10 \
+    --encoder_type resnet-pretrained-18 \
+    --eval_n_trajs 20 \
     --debug
