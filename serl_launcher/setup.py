0f962cb785c1de170705ac15d60ca9ef6a0f525e
youliangtan <tan_you_liang@hotmail.com>
2024-06-17 11:04:52 -0700

    bump up version
    Signed-off-by: youliangtan <tan_you_liang@hotmail.com>

--- serl_launcher/setup.py
+++ serl_launcher/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="serl_launcher",
-    version="0.1.2",
+    version="0.1.3",
     description="library for rl experiments",
     url="https://github.com/rail-berkeley/serl",
     author="auth",
