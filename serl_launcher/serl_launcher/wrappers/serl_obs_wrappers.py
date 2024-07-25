d15015d758b4ab5f4b14cacfe9d357c5a3c56949
nisutte <nisutte@student.ethz.ch>
2024-07-25 12:28:30 +0200

    consider observation and action scaling in augmentation

--- serl_launcher/serl_launcher/wrappers/serl_obs_wrappers.py
+++ serl_launcher/serl_launcher/wrappers/serl_obs_wrappers.py
@@ -52,24 +52,28 @@ class ScaleObservationWrapper(gym.ObservationWrapper):
                  env,
                  translation_scale=100.,
                  rotation_scale=10.,
-                 velocity_scale=10.,
-                 rotational_velocity_scale=10.,
                  force_scale=1.,
                  torque_scale=10.
                  ):
         super().__init__(env)
         self.translation_scale = translation_scale
         self.rotation_scale = rotation_scale
-        self.velocity_scale = velocity_scale
-        self.rotational_velocity_scale = rotational_velocity_scale
         self.force_scale = force_scale
         self.torque_scale = torque_scale
+
+    def scale_wrapper_get_scales(self):
+        return dict(
+            translation_scale=self.translation_scale,
+            rotation_scale=self.rotation_scale,
+            force_scale=self.force_scale,
+            torque_scale=self.torque_scale
+        )
 
     def observation(self, obs):
         obs["state"]["tcp_pose"][:3] *= self.translation_scale
         obs["state"]["tcp_pose"][3:] *= self.rotation_scale
-        obs["state"]["tcp_vel"][:3] *= self.velocity_scale
-        obs["state"]["tcp_vel"][3:] *= self.rotational_velocity_scale
+        obs["state"]["tcp_vel"][:3] *= self.translation_scale
+        obs["state"]["tcp_vel"][3:] *= self.rotation_scale
         obs["state"]["tcp_force"] *= self.force_scale
         obs["state"]["tcp_torque"] *= self.torque_scale
         return obs
