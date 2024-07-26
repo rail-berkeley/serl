94046607ec1fb9a9d846dde2106da6f2fdc47929
nisutte <nisutte@student.ethz.ch>
2024-07-26 12:39:58 +0200

    bugfix to prevent starting velocities

--- serl_robot_infra/robot_controllers/robotiq_controller.py
+++ serl_robot_infra/robot_controllers/robotiq_controller.py
@@ -325,6 +325,7 @@ class RobotiqImpedanceController(threading.Thread):
         success = success and self.robotiq_control.moveJ(self.reset_Q, speed=1., acceleration=0.8)
         self.print(f"[RIC] moving to {self.reset_Q} with moveJ (joint space)", both=self.verbose)
 
+        time.sleep(0.1)     # wait for 100ms
         await self._update_robot_state()
         with self.lock:
             self.target_pos = self.curr_pos.copy()
