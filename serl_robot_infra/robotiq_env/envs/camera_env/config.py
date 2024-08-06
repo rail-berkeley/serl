changed environment step and safety margins to mrp instead of euler, so we do not run into x-before-y rotation stuff at all (mrp is essentially human readable quaternion), huge bugfix

serl_robot_infra/robotiq_env/envs/camera_env/config.py
@@ -56,7 +56,7 @@ class RobotiqCameraConfigBox5(DefaultEnvConfig):
     }
 
 
-class RobotiqCameraConfigFinal(DefaultEnvConfig):       # config for 10 boxes
+class RobotiqCameraConfigFinal(DefaultEnvConfig):  # config for 10 boxes
     RESET_Q = np.array([
         [2.6259, - 1.5196, 2.1287, - 2.1784, - 1.5665, - 0.4741],
         [2.0131, - 1.271, 1.9316, - 2.2306, - 1.566, 0.4537],
@@ -72,8 +72,8 @@ class RobotiqCameraConfigFinal(DefaultEnvConfig):       # config for 10 boxes
     RANDOM_RESET = False
     RANDOM_XY_RANGE = (0.0,)
     RANDOM_RZ_RANGE = (0.0,)
-    ABS_POSE_LIMIT_HIGH = np.array([0.6, 0.1, 0.25, 3.2, 0.18, 3.2])
-    ABS_POSE_LIMIT_LOW = np.array([-0.7, -0.85, -0.006, 2.8, -0.18, -3.2])
+    ABS_POSE_LIMIT_HIGH = np.array([0.6, 0.1, 0.25, 0.05, 0.05, 1.])
+    ABS_POSE_LIMIT_LOW = np.array([-0.7, -0.85, -0.006, -0.05, -0.05, -1.])
     ABS_POSE_RANGE_LIMITS = np.array([0.36, 0.83])
     ACTION_SCALE = np.array([0.02, 0.1, 1.], dtype=np.float32)
 
@@ -98,8 +98,7 @@ class RobotiqCameraConfigFinalTests(RobotiqCameraConfigFinal):
     RANDOM_RZ_RANGE = (0.0,)
 
     RESET_Q = np.array([
-        # [ 0.0421, -1.3161,  1.9649, -2.2358, -1.3221, -1.5237],     # schräge position
-        [0.1882, -1.2777,  1.9699, -2.2983, -1.5567, -1.384]        # gerade pos
+        # [ 0.0421, -1.3161,  1.9649, -2.2358, -1.3221, -1.5237 + 0 * np.pi/2.],     # schräge position
+        # [0.1882, -1.2777, 1.9699, -2.2983, -1.5567, -1.384 + 2 * np.pi / 2],  # gerade pos
+        [0.7431341409683228, -1.0744208258441468, 1.6481602827655237, -2.1433049641051234, -1.5655501524554651, 0.7431478500366211 + 0 * np.pi/2],  # LEGO box
     ])
-    ABS_POSE_LIMIT_HIGH = np.array([0.6, 0.1, 0.25, 3.2, 0.3, 3.2])
-    ABS_POSE_LIMIT_LOW = np.array([-0.7, -0.85, -0.006, 2.6, -0.3, -3.2])

serl_robot_infra/robotiq_env/envs/relative_env.py
@@ -100,7 +100,8 @@ class RelativeFrame(gym.Wrapper):
         using the rotation matrix
         """
         action = np.array(action)  # in case action is a jax read-only array
-        action[:3] = self.rotation_matrix @ action[:3]
+        action[:3] = self.rotation_matrix_reset @ action[:3]
+        action[3:6] = self.rotation_matrix_reset @ action[3:6]
         return action
 
     def transform_action_inv(self, action: np.ndarray):
@@ -109,5 +110,6 @@ class RelativeFrame(gym.Wrapper):
         using the rotation matrix.
         """
         action = np.array(action)
-        action[:3] = self.rotation_matrix.transpose() @ action[:3]
+        action[:3] = self.rotation_matrix_reset.transpose() @ action[:3]
+        action[3:6] = self.rotation_matrix_reset.transpose() @ action[3:6]
         return action

serl_robot_infra/robotiq_env/envs/robotiq_env.py
@@ -20,7 +20,6 @@ from robotiq_env.camera.rs_capture import RSCapture
 from robotiq_env.camera.utils import PointCloudFusion, CalibrationTread
 
 from robotiq_env.utils.real_time_plotter import DataClient
-from robotiq_env.utils.rotations import rotvec_2_quat, quat_2_rotvec, pose2quat, pose2rotvec
 from robot_controllers.robotiq_controller import RobotiqImpedanceController
 
 from serl_launcher.utils.numpy_utils import bool_2_int8
@@ -164,7 +163,7 @@ class RobotiqEnv(gym.Env):
             config.ABS_POSE_RANGE_LIMITS[1],
             dtype=np.float64,
         )
-        self.rpy_bounding_box = gym.spaces.Box(
+        self.mrp_bounding_box = gym.spaces.Box(
             config.ABS_POSE_LIMIT_LOW[3:],
             config.ABS_POSE_LIMIT_HIGH[3:],
             dtype=np.float64,
@@ -279,31 +278,18 @@ class RobotiqEnv(gym.Env):
 
                 self.calibrate_pointcloud_fusion(visualize=True)
 
-    def clip_safety_box(self,
-                        next_pos: np.ndarray) -> np.ndarray:  # TODO make better, no euler -> quat -> euler -> quat
+    def clip_safety_box(self, next_pos: np.ndarray) -> np.ndarray:
         """Clip the pose to be within the safety box."""
         next_pos[:3] = np.clip(
             next_pos[:3], self.xyz_bounding_box.low, self.xyz_bounding_box.high
         )
-        xy_range = np.clip(np.linalg.norm(next_pos[:2], 2), self.xy_range.low, self.xy_range.high)
-        next_pos[:2] = next_pos[:2] / np.linalg.norm(next_pos[:2]) * xy_range
-
-        euler = R.from_quat(next_pos[3:]).as_euler("xyz")
-
-        # Clip first euler angle separately due to discontinuity from pi to -pi
-        sign = np.sign(euler[0])
-        euler[0] = sign * (
-            np.clip(
-                np.abs(euler[0]),
-                self.rpy_bounding_box.low[0],
-                self.rpy_bounding_box.high[0],
-            )
+        orientation_diff = (R.from_quat(next_pos[3:]) * R.from_quat(self.curr_reset_pose[3:]).inv()).as_mrp()
+        print(self.curr_reset_pose[3:], next_pos[3:], orientation_diff, end=" ")
+        orientation_diff = np.clip(
+            orientation_diff, self.mrp_bounding_box.low, self.mrp_bounding_box.high
         )
-
-        euler[1:] = np.clip(
-            euler[1:], self.rpy_bounding_box.low[1:], self.rpy_bounding_box.high[1:]
-        )
-        next_pos[3:] = R.from_euler("xyz", euler).as_quat()
+        next_pos[3:] = (R.from_mrp(orientation_diff) * R.from_quat(self.curr_reset_pose[3:])).as_quat()
+        print(orientation_diff, next_pos[3:])
 
         return next_pos
 
@@ -323,10 +309,9 @@ class RobotiqEnv(gym.Env):
         next_pos = self.curr_pos.copy()
         next_pos[:3] = next_pos[:3] + action[:3] * self.action_scale[0]
 
-        # orientation
         next_pos[3:] = (
-                R.from_quat(next_pos[3:]) * R.from_euler("xyz", action[3:6] * self.action_scale[1])
-        ).as_quat()
+                R.from_mrp(action[3:6] * self.action_scale[1] / 4.) * R.from_quat(next_pos[3:])
+        ).as_quat()             # c * r  --> applies c after r
 
         gripper_action = action[6] * self.action_scale[2]
 

serl_robot_infra/robotiq_env/envs/wrappers.py
@@ -6,7 +6,7 @@ from robotiq_env.spacemouse.spacemouse_expert import SpaceMouseExpert
 import time
 from scipy.spatial.transform import Rotation as R
 
-from robotiq_env.utils.rotations import quat_2_euler
+from robotiq_env.utils.rotations import quat_2_euler, quat_2_mrp
 
 sigmoid = lambda x: 1 / (1 + np.exp(-x))
 
@@ -34,7 +34,8 @@ class SpacemouseIntervention(gym.ActionWrapper):
         """
         expert_a = self.get_deadspace_action()
 
-        if np.linalg.norm(expert_a) > 0.001 or self.left.any() or self.right.any():  # also read buttons with no movement
+        if np.linalg.norm(
+                expert_a) > 0.001 or self.left.any() or self.right.any():  # also read buttons with no movement
             self.last_intervene = time.time()
 
         if self.gripper_enabled:
@@ -54,7 +55,7 @@ class SpacemouseIntervention(gym.ActionWrapper):
         negative = np.clip((expert_a + self.deadspace) / (1. - self.deadspace), a_min=-1.0, a_max=0.0)
         expert_a = positive + negative
 
-        self.left, self.right = np.roll(self.left, -1), np.roll(self.right, -1)     # shift them one to the left
+        self.left, self.right = np.roll(self.left, -1), np.roll(self.right, -1)  # shift them one to the left
         self.left[-1], self.right[-1] = tuple(buttons)
 
         return np.array(expert_a, dtype=np.float32)
@@ -107,6 +108,27 @@ class Quat2EulerWrapper(gym.ObservationWrapper):
         tcp_pose = observation["state"]["tcp_pose"]
         observation["state"]["tcp_pose"] = np.concatenate(
             (tcp_pose[:3], quat_2_euler(tcp_pose[3:]))
+        )
+        return observation
+
+
+class Quat2MrpWrapper(gym.ObservationWrapper):
+    """
+    Convert the quaternion representation of the tcp pose to euler angles
+    """
+
+    def __init__(self, env: gym.Env):
+        super().__init__(env)
+        # from xyz + quat to xyz + euler
+        self.observation_space["state"]["tcp_pose"] = gym.spaces.Box(
+            -np.inf, np.inf, shape=(6,)
+        )
+
+    def observation(self, observation):
+        # convert tcp pose from quat to euler
+        tcp_pose = observation["state"]["tcp_pose"]
+        observation["state"]["tcp_pose"] = np.concatenate(
+            (tcp_pose[:3], quat_2_mrp(tcp_pose[3:]))
         )
         return observation
 